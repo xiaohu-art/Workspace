d8dacfe3c2bd9edb71ba3d04e09e3f4cf6b61e05
liucao <l-cao20@mails.tsinghua.edu.cn>
2025-09-21 15:58:59 +0800

    humanoid_g1_test: largest foot weight for fixed foot frame; com task fot pelvis height, pelvis pose task for pelvis orientation; to be coordinated

--- examples/humanoid_g1_test.py
+++ examples/humanoid_g1_test.py
@@ -51,32 +51,43 @@ def build_qstar_with_joint_targets(model, qpos, targets):
         
     return q_star
 
+def site_world_pose(model, data, site_name):
+    sid = model.site(site_name).id
+    p = data.site_xpos[sid].copy()
+    return p
+
+def body_world_pose(model, data, body_name):
+    bid = model.body(body_name).id
+    p = data.xpos[bid].copy()
+    return p
+
 if __name__ == "__main__":
     model = mujoco.MjModel.from_xml_path(_XML.as_posix())
 
     configuration = mink.Configuration(model)
 
     tasks = [
-        pelvis_orientation_task := mink.FrameTask(
+        pelvis_pose_task := mink.FrameTask(
             frame_name="pelvis",
             frame_type="body",
-            position_cost=5.0,
+            position_cost=0.0,
             orientation_cost=5.0,
             lm_damping=1.0,
         ),
         posture_task := mink.PostureTask(model, cost=5.0),
+        com_task := mink.ComTask(cost=10.0),
         left_foot_task := mink.FrameTask(
             frame_name="left_foot",
             frame_type="site",
-            position_cost=10.0,
-            orientation_cost=1.0,
+            position_cost=100.0,
+            orientation_cost=0.0,
             lm_damping=1.0,
         ),
         right_foot_task := mink.FrameTask(
             frame_name="right_foot",
             frame_type="site",
-            position_cost=10.0,
-            orientation_cost=1.0,
+            position_cost=100.0,
+            orientation_cost=0.0,
             lm_damping=1.0,
         ),
     ]
@@ -87,8 +98,8 @@ if __name__ == "__main__":
     collision_avoidance_limit = mink.CollisionAvoidanceLimit(
         model=model,
         geom_pairs=collision_pairs,  # type: ignore
-        minimum_distance_from_collisions=0.05,
-        collision_detection_distance=0.05,
+        minimum_distance_from_collisions=0.01,
+        collision_detection_distance=0.04,
     )
 
     limits = [
@@ -108,9 +119,11 @@ if __name__ == "__main__":
         # Initialize to the home keyframe.
         configuration.update_from_keyframe("stand")
         posture_task.set_target_from_configuration(configuration)
-        pelvis_orientation_task.set_target_from_configuration(configuration)
+        pelvis_pose_task.set_target_from_configuration(configuration)
+        left_foot_task.set_target_from_configuration(configuration)
+        right_foot_task.set_target_from_configuration(configuration)
 
-        root_height_range = np.arange(0.1, 0.76, 0.01)
+        root_height_range = np.arange(0.2, 0.76, 0.01)
         root_height_range = root_height_range[::-1]
         height_index = 0
 
@@ -119,45 +132,40 @@ if __name__ == "__main__":
 
         rate = RateLimiter(frequency=200.0, warn=False)
         while viewer.is_running():
-            # Update task targets.
-            pelvis_orientation_task.set_target(
-                mink.SE3.from_rotation_and_translation(
+            # Update task targets
+            com_task.set_target(np.array([0, 0, root_height_range[height_index]]))
+            pelvis_pose_task.set_target(
+                mink.SE3.from_rotation(
                     rotation=mink.SO3.from_rpy_radians(0, root_pitch_range[pitch_index], 0),
-                    translation=np.array([0, 0, root_height_range[height_index]]),
                 )
             )
 
-            q_star = build_qstar_with_joint_targets(model, 
-                                                    data.qpos, 
-                                                    {
-                                                        "waist_pitch_joint": 0.0,
-                                                        "waist_roll_joint": 0.0,
-                                                    }
-                                                    )
+            q_star = build_qstar_with_joint_targets(
+                            model, 
+                            data.qpos, 
+                            {   
+                                "left_shoulder_pitch_joint": 0.0,
+                                "left_shoulder_roll_joint": 0.0,
+                                "right_shoulder_pitch_joint": 0.0,
+                                "right_shoulder_roll_joint": 0.0,
+                                "left_elbow_joint": 0.0,
+                                "right_elbow_joint": 0.0,
+                                "waist_pitch_joint": 0.0,
+                                "waist_roll_joint": 0.0,
+                            }
+            )
             data.qpos[:] = q_star
             mujoco.mj_forward(model, data)
             posture_task.set_target(q_star)
-
-            # left foot target: [-0.00142, 0.11851, 0.0383]
-            # right foot target: [-0.00142, -0.11851, 0.0383]
-            left_foot_task.set_target(
-                mink.SE3.from_translation(
-                    translation=np.array([-0.00142, 0.11851, 0.0383]),
-                )
-            )
-            right_foot_task.set_target(
-                mink.SE3.from_translation(
-                translation=np.array([-0.00142, -0.11851, 0.0383]),
-            ))
 
             vel = mink.solve_ik(
                 configuration, tasks, rate.dt, solver, 1e-1, limits=limits
             )
             configuration.integrate_inplace(vel, rate.dt)
-            mujoco.mj_camlight(model, data)
 
             # Note the below are optional: they are used to visualize the output of the
             # fromto sensor which is used by the collision avoidance constraint.
+            mujoco.mj_camlight(model, data)
             mujoco.mj_fwdPosition(model, data)
             mujoco.mj_sensorPos(model, data)
 
